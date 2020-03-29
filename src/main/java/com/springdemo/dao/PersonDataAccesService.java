43ad6a719b8ca58b006d7760d7465e27574b30e3
vladtf <vendeta0263@gmail.com>
2020-03-29 03:00:28 +0300

    Add a second implementation of DAO to test dependency injection features

---
+++ src/main/java/com/springdemo/dao/PersonDataAccesService.java
@@ -0,0 +1,36 @@
+package com.springdemo.dao;
+
+import com.springdemo.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository("postgres")
+public class PersonDataAccesService implements PersonDao {
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return List.of(new Person(UUID.randomUUID(), "From Postgres DB"));
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+}
